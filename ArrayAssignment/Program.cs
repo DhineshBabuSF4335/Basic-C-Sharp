1fe5bd1bfd2f60ca1f9f36ae42cce3a943d907a1
DhineshBabuSF4335 <154953108+DhineshBabuSF4335@users.noreply.github.com>
2023-12-27 11:46:13 +0530

    2450:Update

---
+++ ArrayAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+namespace ARRAYASSIGNMENT;
+
+class Program{
+    public static void Main(String[] args){
+        string [] arr = new string[5];
+        arr[0] = "h";
+        arr[1] = "Dhinesh";
+        arr[2] = "Babu";
+        arr[3] = "vikas";
+        arr[4]  = "kholi0";
+        foreach(string i in arr){
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Enter the name:");
+        string name = Console.ReadLine();
+        int count = 0;
+        for(int i = 0; i<arr.Length; i++){
+            if(arr[i]==name){
+               // Console.WriteLine($"name found in array {name}");
+                //Console.WriteLine($"the index of array is"+ i);
+                count++;
+            }
+            
+        }
+        if(count < 1){
+            Console.WriteLine("The name is not present in array");
+        }else{
+            Console.WriteLine("The name is present in array");
+        }
+        int inc = 0;
+        foreach(string j in arr){
+            if(j==name){
+             inc++;
+            }
+        }
+          if(inc < 1){
+            Console.WriteLine("The name is not present in array");
+        }else{
+            Console.WriteLine("The name is present in array");
+        }
+
+    }
+}
---
+++ Date/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace Date;
+class Program{
+
+    public static void Main(string[] args)
+    {   
+
+        // DateTime date = DateTime.TryParseExact()
+        // bool temp  = DateTime.TryParseExact(Console.ReadLine(),"hh:mm tt",null,System.Globalization.DateTimeStyles.None,out DateTime date);
+        // Console.WriteLine(date);
+
+        Console.WriteLine("Enter the Date of CheckIn format of dd/MM/yyyy:");
+        DateTime inDate;
+        bool temp3 = DateTime.TryParseExact(Console.ReadLine(),"dd/MM/yyyy",null,System.Globalization.DateTimeStyles.None,out inDate);
+        while(!temp3){
+            Console.WriteLine("Enter the Date Correctly:");
+            Console.WriteLine("Enter the Date hh:mm ttt");
+            DateTime.TryParseExact(Console.ReadLine(),"dd/MM/yyyy",null,System.Globalization.DateTimeStyles.None,out inDate);
+
+        }
+        Console.WriteLine(inDate);
+        Console.WriteLine("Enter the Date of CheckOut format of dd/MM/yyyy:");
+        DateTime outDate;
+        bool temp4 = DateTime.TryParseExact(Console.ReadLine(),"dd/MM/yyyy",null,System.Globalization.DateTimeStyles.None,out outDate);
+        while(!temp4){
+            Console.WriteLine("Enter the Date Correctly:");
+            Console.WriteLine("Enter the Date hh:mm ttt");
+            DateTime.TryParseExact(Console.ReadLine(),"dd/MM/yyyy",null,System.Globalization.DateTimeStyles.None,out outDate);
+
+        }
+        Console.WriteLine(outDate);
+         
+
+
+        DateTime checkInTime;
+        Console.WriteLine("Enter the CheckInTime hh:mm ttt");
+        bool temp = DateTime.TryParseExact(Console.ReadLine(),"hh:mm ttt",null,System.Globalization.DateTimeStyles.None,out checkInTime);
+        while(!temp){
+            Console.WriteLine("Enter the Date Correctly:");
+            Console.WriteLine("Enter the Date hh:mm ttt");
+            DateTime.TryParseExact(Console.ReadLine(),"hh:mm ttt",null,System.Globalization.DateTimeStyles.None,out checkInTime);
+
+        }
+        Console.WriteLine(checkInTime);
+                DateTime checkOutTime;
+        Console.WriteLine("Enter the CheckOutTime hh:mm ttt");
+        bool temp1  = DateTime.TryParseExact(Console.ReadLine(),"hh:mm ttt",null,System.Globalization.DateTimeStyles.None,out checkOutTime);
+        while(!temp1){
+            Console.WriteLine("Enter the Date Correctly:");
+            Console.WriteLine("Enter the Date hh:mm ttt");
+            DateTime.TryParseExact(Console.ReadLine(),"hh:mm ttt",null,System.Globalization.DateTimeStyles.None,out checkOutTime);
+
+        }
+        Console.WriteLine(checkOutTime);
+
+        TimeSpan time = checkOutTime - checkInTime;
+        int totalHours = time.Hours;
+
+        if(inDate == outDate && totalHours >= 8){
+
+        }else{
+            Console.WriteLine("Input Invalid..");
+        }
+    }
+}
